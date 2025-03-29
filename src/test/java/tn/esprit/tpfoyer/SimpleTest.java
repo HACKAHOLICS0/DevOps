bebe39c65f027d61d113bdb66cd686f25bbf2dde
Your Name <khalil.benyahiawenich@esprit.tn>
2025-03-29 03:57:48 +0100

    Ajout test unitaire

---
+++ src/test/java/tn/esprit/tpfoyer/SimpleTest.java
@@ -0,0 +1,11 @@
+package tn.esprit.tpfoyer;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTest {
+    @Test
+    void shouldPass() {  // Note: méthode de test doit être publique pour JUnit 5
+        assertTrue(true);
+    }
+}
