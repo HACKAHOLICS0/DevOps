9e772fff6e63421c9bd1743a1ddf94ca351d0236
firaszn <mohamedfiras.zighni@esprit.tn>
2025-04-14 01:03:37 +0100

    ajout de services avancées de test

--- src/main/java/tn/esprit/tpfoyer/service/BlocServiceImpl.java
+++ src/main/java/tn/esprit/tpfoyer/service/BlocServiceImpl.java
@@ -20,6 +20,7 @@ public class BlocServiceImpl  implements IBlocService {
 
 
     BlocRepository blocRepository;
+    FoyerRepository foyerRepository;
 
     @Scheduled(fixedRate = 30000) // millisecondes // cron fixedRate
     //@Scheduled(cron="0/15 * * * * *")
